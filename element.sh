4942f219e94b293e180a4e74c99d097f1ac40eb3
mounesi <armsohi@gmail.com>
2023-04-13 22:37:01 +0000

    feat: adding feature for getting 1 H Hydrogen

--- element.sh
+++ element.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo Please provide an element as an argument.
+
+    
 
 function display_element_info {
-  local elelment_name=$1
+  local element_name=$1
   local atomic_number=$2
   local atomic_mass=$3
   local melting_point=$4
@@ -15,3 +16,24 @@ function display_element_info {
   echo "The element with atomic number $atomic_number is $element_name ($symbol_name). It's a $type_name, with a mass of $atomic_mass amu. $element_name has a melting point of $melting_point celsius and a boiling point of $boiling_point celsius." 
 }
 
+# Check if a parameter was passed to the script
+if [ -z "$1" ]; then
+    # No input - do something
+    echo Please provide an element as an argument.
+else
+    # Input provided - do something else
+    #echo "Input provided: $1"
+
+    # Check if the parameter is a valid input
+    case "$1" in
+        1|H|Hydrogen)
+            # Display the element information
+            display_element_info "Hydrogen" "1" "1.008" "-259.1" "-252.9" "nonmetal" "H"
+            ;;
+        *)
+            # Invalid input - display an error message
+            echo "I could not find that element in the database."
+            ;;
+    esac
+
+fi
